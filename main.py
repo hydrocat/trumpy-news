845a9dff4e027d0400ace5a1bd8db00a54e37280
hydrocat <vdrbandeiras@gmail.com>
2020-02-05 14:56:31 +0200

    fix gunicorn to run on heroku

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from cnn import *
 from twitter import *
 from flask import Flask, render_template
 from news import SimpleArticle as Article
+
 cnn = Cnn(Article)
 twitter = Twitter(Article)
+application = Flask(__name__)
 
-app = Flask(__name__)
-
-@app.route('/')
-def main():
+@application.route('/')
+def main(*args):
     news = twitter.get_tweets()
     news.extend(cnn.get_news())
     news.sort(key = lambda article: article.time_published, reverse = True)
